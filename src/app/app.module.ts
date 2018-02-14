06daca0c944177e1fdbfa6d6760d5bfd6fd56486
babajan <babajan2009@gmail.com>
2018-02-14 22:28:30 +0530

    Added header/footer/body components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './App_Load/app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { BodyComponentComponent } from './body-component/body-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    BodyComponentComponent
   ],
   imports: [
     BrowserModule
